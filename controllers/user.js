893f02d2871c7af44309c7c9510fc5c7d4e0187f
Brightmuk <be.rightmuk@gmail.com>
2024-04-22 11:21:29 +0300

    Ussd method to send request

--- controllers/user.js
+++ controllers/user.js
@@ -93,7 +93,7 @@ exports.getAdd = async(req, res, next) => {
     const snapshot = await admin.firestore() 
     .collection('members').where('name','!=',req.session.user.name)
     .get();
-    const data = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data(),savingsRange:doc.data().savings < 400 ? "below 400k" : 
+    const data = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data(),savingsRange:doc.data().savings < 400000 ? "below 400k" : 
     doc.data().savings >= 400000 && doc.data().savings < 1000000 ? "400k-1M" :
     doc.data().savings >= 1000000 ? "above 1M" : "Unknown" }));
 
@@ -125,7 +125,7 @@ exports.postAdd = async(req, res, next) => {
         'portfolio':{ 
             'guarantees':currentUser.guarantees,
             'repaymentRate':currentUser.repayment,
-            'savingsRange': currentUser.savings < 400 ? "below 400k" : 
+            'savingsRange': currentUser.savings < 400000 ? "below 400k" : 
                currentUser.savings >= 400000 && currentUser.savings < 1000000 ? "400k-1M" :
                currentUser.savings >= 1000000 ? "above 1M" : "Unknown"
 
@@ -207,7 +207,7 @@ exports.postApprove = async(req, res, next) => {
             }else{
                 var userDoc = query.docs[0].data();
 
-                response = `CON  Hi  ${userDoc.name},  welcome to Sacco Gaurantor Service \n\n`
+                response = `CON  Hi  ${userDoc.name},  welcome to Sacco Guarantor Service \n\n`
                 response+="Please select an option to continue\n"
                 response+="1. Login\n"
                 response+="2. Contact support";
@@ -227,11 +227,11 @@ exports.postApprove = async(req, res, next) => {
                  
             }else{
                 var userDoc = query.docs[0].data();
-                response = `CON ${userDoc.name}\nCurrent Savings: ${userDoc.savings < 400 ? "below 400k" : 
+                response = `CON [${query.docs[0].id}] ${userDoc.name}\nCurrent Savings: ${userDoc.savings < 400000 ? "below 400k" : 
                 userDoc.savings >= 400000 && userDoc.savings < 1000000 ? "400k-1M" :
                 userDoc.savings >= 1000000 ? "above 1M" : "Unknown" }`
 
-                response += `\nGuarantees: ${userDoc.guarantees}\nLoan repayment rate: ${userDoc.repayment},\nCurrent Loans: ${userDoc.loans}\n\n `
+                response += `\nGuarantees: ${userDoc.guarantees}\nLoan repayment rate: ${userDoc.repayment}\nCurrent Loans: ${userDoc.loans}\n\n `
                 response+="Please select an option to proceeed:\n"
 
                 response+="1. Get guarantors\n"
@@ -239,11 +239,93 @@ exports.postApprove = async(req, res, next) => {
 
             }
         }else if(/^1\*\d{4}\*1$/.test(input)){
-                response="CON Getting guarantor"
+            const snapshot = await admin.firestore() 
+            .collection('members').where('phone','!=',phoneNumber)
+            .limit(5)
+            .get();
+            const data = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data(),savingsRange:doc.data().savings < 400000 ? "below 400k" : 
+            doc.data().savings >= 400000 && doc.data().savings < 1000000 ? "400k-1M" :
+            doc.data().savings >= 1000000 ? "above 1M" : "Unknown" }));
+            
+            response="CON Here is a list of suggested guarantors\n\n"
+            
+            for(var i=0;i<data.length;i++){
+                response+=`[${data[i].id}] ${data[i].name}
+           Loans: ${data[i].loans}
+           Loan repayment rate: ${data[i].repayment}%  
+           Guarantees ${data[i].guarantees} 
+           Savings: ${data[i].savings < 400000 ? "below 400k" : 
+                data[i].savings >= 400000 && data[i].savings < 1000000 ? "400k-1M" :
+                data[i].savings >= 1000000 ? "above 1M" : "Unknown" }`
+                response+="\n\n"
+                
+            }
+            response+="\n\nEnter the member number to continue"
+            
+
         }else if(/^1\*\d{4}\*2$/.test(input)){
             response="CON Viewing requests"
-        }else{
-            response="CON Please Select a valid option "
+        }else if(/^1\*\d{4}\*1\*\d{3}$/.test(input)){
+            var memberNo = input.slice(-3)
+            
+            const snapshot = await admin.firestore().doc('members/'+memberNo).get();
+            var userDoc = snapshot.data();
+            if(userDoc!=undefined){
+                response=`CON Request [${memberNo}] ${userDoc.name} to be your guarantor\n\n`
+                response+=`Loans: ${userDoc.loans}\nLoan repayment rate: ${userDoc.repayment}%\nGuarantees ${userDoc.guarantees}`
+                response+=`\nSavings: ${userDoc.savings < 400000 ? "below 400k" : 
+                     userDoc.savings >= 400000 && userDoc.savings < 1000000 ? "400k-1M" :
+                     userDoc.savings >= 1000000 ? "above 1M" : "Unknown" }`
+                     response+="\n\n" 
+                response+="Select 1 to confirm"
+            }
+            
+            
+        }else if(/^1\*\d{4}\*1\*\d{3}\*1$/.test(input)){
+            var memberNo = input.slice(-5,-2)
+
+            const query = await admin.firestore().collection('members').where('phone','==',phoneNumber).get();
+            var senderDoc = query.docs[0].data();
+
+            const snapshot = await admin.firestore().doc('members/'+memberNo).get();
+            var receiverDoc = snapshot.data();
+
+            var resourceId;
+            await admin.firestore()
+            
+            .collection('requests')
+            .add({
+                'amount':'10000000',
+                'to':{ 
+                    'name':receiverDoc.name,
+                    'id':snapshot.id,
+                    'phone':receiverDoc.phone
+                },
+                'date':Timestamp.now(),
+                'requester': senderDoc.name,
+                'status':'review',
+                'portfolio':{ 
+                    'guarantees':senderDoc.guarantees,
+                    'repaymentRate':senderDoc.repayment,
+                    'savingsRange': senderDoc.savings < 400000 ? "below 400k" : 
+                       currentUser.savings >= 400000 && currentUser.savings < 1000000 ? "400k-1M" :
+                       currentUser.savings >= 1000000 ? "above 1M" : "Unknown"
+        
+                },
+            }).then(function(docRef) {
+                resourceId = docRef.id;
+                console.log("Document written with ID: ", docRef.id);
+            })
+
+            if(receiverDoc!=undefined&&senderDoc!=undefined&&resourceId!=undefined){
+                sendSMS(`Hello ${receiverDoc.name}, ${senderDoc.name} from Hauna Hela nini sacco is requesting you to become their guaranter. Visit this link to review their request.\nhttps://guarantor-85ri.onrender.com/review/${resourceId}`,receiverDoc.phone)
+                response="END Request sent to the member"
+            }else{
+                response="END Sorry, there was an error!"
+            }
+           
+            }else{
+            response="END Please Select a valid option"
         }
         console.log("Sending response: ",response)
         res.send(response);
